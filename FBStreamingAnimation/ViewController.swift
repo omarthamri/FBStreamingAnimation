672e3503b5d7307f6ff402a4b2985789ab550877
MACBOOK PRO RETINA <macbookproretina@MacBook-Pro-de-MACBOOK-2.local>
2018-11-25 00:01:15 +0100

    first commit

---
+++ FBStreamingAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBStreamingAnimation
+//
+//  Created by MACBOOK PRO RETINA on 22/11/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
