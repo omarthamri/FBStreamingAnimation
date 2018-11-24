88995dd790f41c5ddc19e5fa521f9f901cc8f7bf
MACBOOK PRO RETINA <macbookproretina@MacBook-Pro-de-MACBOOK-2.local>
2018-11-25 00:02:28 +0100

    update code

--- FBStreamingAnimation/ViewController.swift
+++ FBStreamingAnimation/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var secondsSinceLastGeneration = 10
+    let stop = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playVideo(from: "no-excuses-best-motivational-video.mp4")
+        tick()
+    }
+    
+    func tick() {
+       // secondsSinceLastGeneration += 1
+        if  secondsSinceLastGeneration >= 10 {
+            (0...10).forEach{ (_) in
+            GenerateAnimateViews()
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: {
+            self.tick()
+        })
+    }
+    
+    func GenerateAnimateViews() {
+        let chosenImage = drand48() > 0.5 ? #imageLiteral(resourceName: "like") : #imageLiteral(resourceName: "love")
+        let imageView = UIImageView(image: chosenImage)
+        imageView.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = CreateBezierPath().cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.isRemovedOnCompletion = false
+        imageView.layer.add(animation, forKey: nil)
+        view.addSubview(imageView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func playVideo(from file:String) {
+        let file = file.components(separatedBy: ".")
+        
+        guard let path = Bundle.main.path(forResource: file[0], ofType:file[1]) else {
+            debugPrint( "\(file.joined(separator: ".")) not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height/2)
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
     }
-
-
+    
 }
+
+func CreateBezierPath() -> UIBezierPath {
+    let path = UIBezierPath()
+    path.move(to: CGPoint(x: 0, y: 200))
+    let randomY = 200 + drand48() * 300
+    path.addCurve(to: CGPoint(x: UIScreen.main.bounds.width, y: 200), controlPoint1: CGPoint(x: UIScreen.main.bounds.width / 4, y: CGFloat(100 - randomY)), controlPoint2: CGPoint(x: UIScreen.main.bounds.width / 2, y: CGFloat(300 + randomY)))
+    return path
+}
+
 
